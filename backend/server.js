2466d3608935630c3574b8caed3217857a60df4b
Aqeel Nazar <aqeelnazar212@gmail.com>
2026-07-05 18:50:52 +0530

    Add server.js for backend API setup and routing

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+const connectDB = require("./config/db");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
+const authRoutes = require("./routes/authRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.get("/", (req, res) => {
+  res.send("Online Shop API is running");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
