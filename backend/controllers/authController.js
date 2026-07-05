d6ed2f3fbf8aa44c0a160cb9e92526dec958e446
Aqeel Nazar <aqeelnazar212@gmail.com>
2026-07-05 18:51:02 +0530

    Add authController.js for user registration and login functionality

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+      return;
+    }
+
+    res.status(401);
+    throw new Error("Invalid email or password");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
